36a2c7e692bb16e163538ba73953be1f516377da
Benjascaf <benjascaf@gmail.com>
2024-04-01 23:46:16 -0300

    Hasta el 6

--- practica1.py
+++ practica1.py
@@ -1,5 +1,5 @@
 import time 
-
+import np
 # Ej 1
 #a) (Comsultar)
 #Las soluciones candidatas serian aquellas tuplas de tres elementos cuyos vaores pueden ser 1 o 0
@@ -162,8 +162,7 @@ def count_magic_squares(square, i,j):
 
 
 #Ej 3
-#a
-from collections import deque
+#a No me guista como lo implemente
 def total(matrix: list[list[int]], subset: list[int]):
     total: int = 0
     for i in subset:
@@ -171,25 +170,193 @@ def total(matrix: list[list[int]], subset: list[int]):
             total += matrix[i - 1][j - 1]
     return total
 
-current_max = [[], 0]
-def maxi_subconjunto(matrix: list[list[int]], k: int, subset: list[int], i:int):
-    if len(subset) == k:
+#Makin 2^n recursive calls
+def maxi_subconjunto(matrix: list[list[int]], k: int, subset: list[int], i:int, current_max = [[], 0]):
+    if len(subset) == k: #O(k)
         # print(subset)
-        global current_max
-        if total(matrix, subset) > current_max[1]:
+        if total(matrix, subset) > current_max[1]: #O(k^2)
             current_max[0] = subset.copy()
             current_max[1] = total(matrix, subset)
-            return 
-    elif i <= len(matrix):
+            return current_max[0]
+    elif i <= len(matrix): #O(n)
         subset.append(i)
         maxi_subconjunto(matrix, k, subset, i + 1)
         subset.pop()
         maxi_subconjunto(matrix, k, subset, i + 1)
+        return current_max[0]
 
-maxi_subconjunto([
+s = maxi_subconjunto([
     [0,10,10,1],
     [10,0,5,2],
-    [10,5,0,1],
+    [10,5,1,1],
     [1,2,1,0]
-    ], 3, [], 1)
-print(current_max)
+    ], 2, [], 1)
+
+# print("Prueba 1")
+M = [ [3,1,2,4],
+                    [1,4,3,5],
+                    [2,3,5,6],
+                    [4,5,6,6] ] # [1,2,3]
+
+# print(maxi_subconjunto(M, 3, [], 1))
+
+
+#b
+#Si nom me quivoco, en pero caso se hacen un total de 2^n llamados recursivos, 
+# cada uno con complejidad O(k)  + O(n) + O(k^2) (Este ultimo no estoy seguro pq solo lo haces en las hojas, como tengo esto en cuenta?)
+#Luego, la complejidad quedaria O(2^n(k^2 + n))
+
+#Noc como calcular complejidad espacial
+
+
+#Ej 4 
+
+def calculate_sum(matrix: list[list[int]], permutation: list[int]):
+    n = len(matrix) - 1
+    total = matrix[permutation[n] - 1][permutation[0] - 1]
+    for i in range(n):
+        total += matrix[permutation[i] - 1][permutation[i+1] - 1]
+    return total
+
+def replaceElements(original: list[any], replacement: list[any]):
+    for i in range(len(original)):
+        original[i] = replacement[i]
+def rutaMinima(matrix: list[list[int]], curr_permutation: list[int], min_permutation: list[int], present_numbers: dict[int, bool], missing_numbers:list[bool]):
+    n = len(matrix)
+    if not missing_numbers:
+        missing_numbers += [n for n in range(1, len(matrix) + 1)]
+    if len(curr_permutation) == n:
+        total = calculate_sum(matrix, curr_permutation)
+        if total < calculate_sum(matrix, min_permutation):
+            replaceElements(min_permutation, curr_permutation)
+    else:
+        for num in missing_numbers:
+            if num not in present_numbers:
+                present_numbers[num] = True 
+                curr_permutation.append(num)
+                rutaMinima(matrix, curr_permutation, min_permutation, present_numbers, missing_numbers)
+                curr_permutation.pop()
+                rutaMinima(matrix, curr_permutation, min_permutation, present_numbers, missing_numbers)
+                del present_numbers[num]
+min_permutation = [n for n in range (4,0, -1)]
+# print(min_permutation)
+# Ejemplo = np.array([[0, 1, 10, 10],
+#                         [10, 0, 3, 15],
+#                         [21, 17, 0, 2],
+#                         [3, 22, 30, 0]]).T
+
+# rutaMinima(Ejemplo, [], min_permutation, {}, [])
+# print(min_permutation)
+
+#Ej 5
+#c
+M = [[None for _ in range(0, 34 + 1)] for _ in range(0, 14 + 1)]
+
+def ss_top(c, i, j):
+    if j < 0:
+        return False
+        
+    if i == 0:
+        return j == 0
+    elif not M[i][j]:
+        M[i][j] = ss_top(c, i - 1, j) or ss_top(c, i - 1, j - c[i - 1])
+    else:
+        print(M[i][j])
+    return M[i][j]
+#f
+M = []
+def ss_bottom(c, k):
+    global M
+    if not M:
+        M = [[None for _ in range(k+1)] for _ in range(len(c) + 1)]
+        for col in range(k+1):
+            M[0][col] = col == 0
+    for i in range(1, len(c) + 1):
+        for j in range(k  +1):
+            print(i, j)
+            if j >= c[i - 1]:
+                M[i][j] = M[i-1][j] or M[i-1][j - c[i- 1]]
+            else:
+                M[i][j] = M[i-1][j]
+
+#g Esta mal, noc xomo evitar tener siempre dos listas que representen la fila actual y la anterior
+    #pq si uso solo uno modifico las posiciones lo que termina afectando calculos posteriores
+    #Esto funca, pero no me convence
+def ss_bottom_more_betterer(c, k):
+    prev_row = [n == 0 for n in range (k+1)]
+    # curr_row = [None for _ in range(k + 1)]
+    for i in range(1, len(c) + 1):
+        for j in range(k + 1):
+            if j >= c[i - 1]:
+                prev_row[j] = prev_row[j] or prev_row[j - c[i -1 ]]
+            else:
+                prev_row[j] = prev_row[j]
+        # prev_row = curr_row.copy()
+    return prev_row[j]
+
+# print(ss_bottom_more_betterer([6,11,3, 5, 5, 5], 7))
+
+
+#Ej 6 
+
+#O(2^n) porque se realizan 2^n llamados recursivos y cada una cuesta O(1)
+def min_cc(fst: list[int, int], snd:list[int, int]):
+    if fst[0] == snd[0]:
+        if fst[1] < snd[1]:
+            return fst
+        else: 
+            return snd
+    return fst if fst[0] < snd[0] else snd
+def cc(B: list[int], i: int, j: int) -> list[int, int]:
+    if j <= 0:
+        return [0,0]
+    elif i == 0:
+        return [float("inf"), float("inf")]
+    else:
+        without_last =  cc(B, i - 1, j)
+        with_last = cc(B, i - 1, j - B[i - 1])
+        with_last[1] += 1
+        with_last[0] += B[i - 1]
+        return min_cc(without_last, with_last)
+    
+
+R = []
+#e
+#O(nc)? pq evito repeticion de estados?
+def ccp(B: list[int], i: int, j: int) -> list[int, int]:
+    global R
+    if not R:
+        R = [[None for _ in range(j + 1)] for _ in range(i + 1)]
+    if j <= 0:
+        return [0,0]
+    elif i == 0:
+        R[i][j] = [float("inf"), float("inf")]
+    elif not R[i][j]:
+        without_last =  cc(B, i - 1, j)
+        with_last = cc(B, i - 1, j - B[i - 1])
+        with_last[1] += 1
+        with_last[0] += B[i - 1]
+        R[i][j] =  min_cc(without_last, with_last)
+    return R[i][j]
+
+
+#f Anda pero no me gusta, pero tampoco se como hacer para usar solo una lista, 
+# porque si lo hago asi tengo q cambio los valores de posiciones q puedo llegar a usar dsp
+def ccp_bottom(B: list[int], i:int, j:int) -> list[int, int]:
+    curr = [[float("inf"), float("inf")] for _ in range(j + 1)]
+    prev = R.copy()
+    R[0] = [0,0]
+    for i in range(i, 0, -1):
+        for j in range(1, j + 1):
+            resta = j - B[i -1] if j >= B[i - 1] else 0
+            fst = prev[j].copy()
+            snd = prev[resta].copy()
+            snd[0] += B[i - 1]
+            snd[1] += 1
+            curr[j] = min_cc(fst, snd)
+        prev = curr.copy()
+    return curr[j]
+
+print(ccp_bottom([2, 3, 5, 10, 20, 20], 6, 14))
+
+#g Ej de demostracion consultar
